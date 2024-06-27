367655e4667796038fcfdccd408acce8fd488591
Margaux Doisy <margauxdoisy@gmail.com>
2024-06-27 14:42:31 +0200

    ticket DAO

--- src/main/java/org/example/DAO/TicketDAO.java
+++ src/main/java/org/example/DAO/TicketDAO.java
@@ -1,6 +1,11 @@
 package org.example.DAO;
 
+import org.example.entity.Address;
+import org.example.entity.Ticket;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 public class TicketDAO {
 
@@ -9,5 +14,59 @@ public class TicketDAO {
     public TicketDAO(EntityManager entityManager) {
         this.em = em;
     }
+
+    public void createTicket(Ticket ticket){
+        em.getTransaction().begin();
+        em.persist(ticket);
+        em.getTransaction().commit();
+    }
+
+    public void displayTicket(int id){
+
+        Query query = em.createQuery("select t from Ticket t where t.id = :id",Ticket.class);
+        query.setParameter("id",id);
+        List<Ticket> tickets = query.getResultList();
+        for(Ticket ticket : tickets){
+            System.out.println(tickets);
+        }
+        try {
+            Ticket ticket1 = em.find(Ticket.class,id);
+            System.out.println(ticket1);
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public void updateTicket(Ticket ticket){
+
+        try {
+            em.getTransaction();
+            em.merge(ticket);
+            em.getTransaction().commit();
+            System.out.println("ticket updated !");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    public void deleteTicket(int id){
+
+        try{
+            em.getTransaction().begin();
+            Ticket ticket = em.find(Ticket.class,id);
+            if (ticket!= null){
+                em.remove(ticket);
+                em.getTransaction().commit();
+                System.out.println("Ticket deleted!!");
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
-//c
